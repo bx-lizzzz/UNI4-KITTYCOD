7199c8f81e3a31abcdbded48dd6e270e57fa6506
bx-lizzzz <besfariamavares@crackthecode.la>
2025-11-22 17:59:52 -0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// Configuración de Vite + Tailwind + React
 export default defineConfig({
-  base: '/Proyecto-Final-U3/', // <--- Clave para que GitHub Pages cargue correctamente
+  base: '/KITTY-CODE-UNI4/',  
   plugins: [react(), tailwindcss()],
 });
-
